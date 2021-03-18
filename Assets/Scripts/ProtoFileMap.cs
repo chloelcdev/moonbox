09e9110dde7dc1d4459fe5b07a62a09f550b8573
Chloe <chloe@sixwingstudios.com>
2021-03-18 00:26:50 -0400

    Update ProtoFileMap.cs

--- Assets/Scripts/ProtoFileMap.cs
+++ Assets/Scripts/ProtoFileMap.cs
@@ -15,20 +15,24 @@ public class ProtoFile
 }
 
 
-public static class ProtoFileMap
+public class ProtoFileMap
 {
     public static List<ProtoFile> Files { get; set; }
 
-    public static void LoadFileMap()
+    public static void SaveFileMap(ProtoFileMap _fileMap)
     {
         using (var file = File.Create("filemap.bin"))
         {
-            Serializer.Serialize<ProtoFile>(file, person);
+            Serializer.Serialize<ProtoFileMap>(file, _fileMap) ;
         }
     }
 
-    public static void SaveFileMap()
+    public static void LoadFileMap()
     {
-
+        ProtoFileMap Files;
+        using (var file = File.OpenRead("filemap.bin"))
+        {
+            Files = Serializer.Deserialize<ProtoFileMap>(file);
+        }
     }
 }
